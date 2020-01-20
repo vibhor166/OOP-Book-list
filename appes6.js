1b3321d8f43930411537ebafe08ebd2011c84ae7
vibhor166 <52551223+vibhor166@users.noreply.github.com>
2020-01-20 16:17:44 -0500

    first commit

---
+++ appes6.js
@@ -0,0 +1,103 @@
+class Book {
+    constructor(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+class UI{
+    addBookToList(book){
+        const list = document.getElementById('book-list');
+        const row =document.createElement('tr');
+        row.innerHTML = `<td>${book.title}</td>
+                     <td>${book.author}</td>
+                     <td>${book.isbn}</td>
+                     <td><a href = "#" class="delete">X</a></td>`;
+        list.appendChild(row);
+    }
+    showAlert(message,className){
+        const div = document.createElement('div');
+        div.className=`alert ${className}`;
+        div.appendChild(document.createTextNode(message)); 
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div,form);
+        setTimeout(function(){
+            document.querySelector('.alert').remove();
+        },3000)
+    }
+    deleteBook(target){
+        if(target.className==='delete'){
+            target.parentElement.parentElement.remove();
+        }
+    }
+    clearFields(){
+        document.getElementById('title').value='';
+        document.getElementById('author').value='';
+        document.getElementById('isbn').value='';
+    }
+}
+//Local Storage Class, because its a static method we do not have to instantiate
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books')===null){
+            books = [];
+        } else{
+            books=JSON.parse(localStorage.getItem('books'))
+        }
+        return books;
+        }
+
+    static displayBooks(){
+            const books = Store.getBooks();
+            books.forEach(function(book){
+            const ui = new UI;
+            ui.addBookToList(book);
+            });
+    }
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach(function(book,index){
+            if(book.isbn===isbn){
+                books.splice(index,1)
+            }
+            });
+        localStorage.setItem('books',JSON.stringify(books));
+        
+    } 
+}
+//DOM Load Event
+ document.addEventListener('DOMContentLoaded',Store.displayBooks)
+ document.getElementById('book-form').addEventListener('submit',function(e){
+    const title = document.getElementById('title').value;
+    const author=document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;  //Instantiate Book
+    const book = new Book (title,author,isbn);   
+    // Instantiate UI
+    const ui = new UI();
+    if (title==='' || author==='' || isbn===''){
+        ui.showAlert('Please enter all fields','error');
+    }
+    else {
+        ui.showAlert('Book Added','success');
+        ui.addBookToList(book);
+        Store.addBook(book);
+        ui.clearFields(); 
+    }  
+    e.preventDefault();
+}); 
+
+document.getElementById('book-list').addEventListener('click',function(e){
+    const ui = new UI();    
+    ui.deleteBook(e.target);
+    //Remove from Local Storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
+    ui.showAlert('Book deleted','success');
+    e.preventDefault();
+});
